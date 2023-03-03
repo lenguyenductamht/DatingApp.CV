46aea196d1f2df1e3539ff17b70cfb63c49967e0
lenguyenductamht <lenguyenductamht@gmail.com>
2023-03-03 15:05:24 +0700

    Initial

---
+++ DatingApp/API/Data/DataContext.cs
@@ -0,0 +1,61 @@
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : IdentityDbContext<AppUser, AppRole, int,
+        IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>,
+        IdentityRoleClaim<int>, IdentityUserToken<int>>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Connection> Connections { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.User)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+
+            builder.Entity<AppRole>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+
+            builder.Entity<UserLike>()
+                .HasKey(x => new { x.SourceUserId, x.TargetUserId });
+            builder.Entity<UserLike>()
+                .HasOne(x => x.SourceUser)
+                .WithMany(x => x.LikedUsers)
+                .HasForeignKey(x => x.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.Entity<UserLike>()
+                .HasOne(x => x.TargetUser)
+                .WithMany(x => x.LikedByUsers)
+                .HasForeignKey(x => x.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Message>()
+                .HasOne(x => x.Recipient)
+                .WithMany(x => x.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(x => x.Sender)
+                .WithMany(x => x.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ DatingApp/client/src/app/members/member-message/member-message.component.ts
@@ -0,0 +1,35 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Message } from 'src/app/_models/message';
+import { MessageService } from 'src/app/_services/message.service';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-member-message',
+  templateUrl: './member-message.component.html',
+  styleUrls: ['./member-message.component.css'],
+})
+export class MemberMessageComponent implements OnInit {
+  @ViewChild('messageForm') messageForm?: NgForm;
+  @Input() username?: string;
+  messageContent = '';
+
+  constructor(public messageService: MessageService) {}
+
+  ngOnInit(): void {}
+
+  sendMessage() {
+    if (!this.username) return;
+    this.messageService
+      .sendMessage(this.username, this.messageContent)
+      .then(() => {
+        this.messageForm?.reset();
+      });
+  }
+}
